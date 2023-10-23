2df015f46e5e15daf73232c00efaea8b2f56ed46
Sobid <rustamovsobid95@gmail.com>
2023-10-23 16:42:20 +0500

    initial and finish

---
+++ app.js
@@ -0,0 +1,37 @@
+alert("O'zingiz haqingizda ma'lumot bersangiz!")
+confirm("Ma'lumot kiritishga tayyormisiz");
+let ism = prompt("Ismingiz");
+let familiya = prompt("Familiyangiz");
+let yosh = prompt("Yoshingiz");
+let ishJoyi = prompt("Qayerda ishlaysiz");
+let kasb = prompt("Qaysi kasb egasisiz");
+let malumot = prompt("Ta'lim darajangizni kiriting 'masalan oliy'")
+let email = prompt("Emailingizni kiriting");
+let tel = prompt("Telefon raqamingizni kiriting");
+let manzil = prompt("Yashash manzilingizni kiriting");
+let tg = prompt("telegram usernameingizni kiriting");
+let til = prompt("Qaysi tillarni bilasiz 'vergul bilan ajratib yozing'");
+
+console.log(
+  "Mening ismim: " +
+    ism +
+    "\nFamiliyam: " +
+    familiya +
+    "\nYoshim: " +
+    yosh +
+    "\nIsh joyim: " +
+    ishJoyi +
+    "\nKasbim: " +
+    kasb + 
+    "\nMa'lumotim: " + 
+    malumot + 
+    "\nEmail manzilim: " + 
+    email + 
+    "\nTel raqamim: " + 
+    tel + 
+    "\nYashash manzilim: " + 
+    manzil + 
+    "\nQuyidagi tillarni bilaman: " + 
+    til + 
+    "\n"
+);
